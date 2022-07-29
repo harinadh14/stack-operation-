f19fc3b0e46bad028d7919cdecd204599f5f8822
harinadh14 <101285437+harinadh14@users.noreply.github.com>
2022-07-29 22:25:26 +0530

    Create stackop.java

---
+++ stackop.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class stackop
+{
+public static void main(String[] args)
+  {
+  Scanner sc = new Scanner( System.in);
+  int x =0 ;
+  Stack<Integer> s = new Stack<>();
+  int top  = 0 ;
+  while (x != 4)
+  {
+    System.out.println("\n 1.push \n 2.pop \n 3.display stack \n 4. exit ");
+    System.out.print("enter the value from above to perform  :");
+    x = sc.nextInt();
+    switch(x)
+    {
+      case 1:
+       {
+         System.out.print("enter value to push ");
+         int y = sc.nextInt();
+         s.push(y);
+         top = top+1;
+         System.out.println("item pushed");
+         break ;
+          }
+      case 3:
+       {
+         System.out.println("your stack is :"+ s );
+         break ;
+       }
+      case 4:
+       {
+         System.out.println("exiting ");
+         System.exit(0);
+         break;
+       }
+
+
+      }
+    }
+
+  }
+}
